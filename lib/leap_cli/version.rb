20ab16c583a139ff64b83ad5450257ca1dc5924e
elijah <elijah@riseup.net>
2012-10-27 15:14:03 -0700

    bump version to 0.1.0

--- lib/leap_cli/version.rb
+++ lib/leap_cli/version.rb
@@ -1,6 +1,6 @@
 module LeapCli
   unless defined?(LeapCli::VERSION)
-    VERSION = '0.0.1'
+    VERSION = '0.1.0'
     SUMMARY = 'Command line interface to the LEAP platform'
     DESCRIPTION = 'Provides the command "leap", used to manage a bevy of servers running the LEAP platform from the comfort of your own home.'
   end
