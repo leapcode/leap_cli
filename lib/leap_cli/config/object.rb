38e87145dee1060e24ec679c2e25fdde14e13c37
elijah <elijah@riseup.net>
2012-11-28 10:29:36 -0800

    fixed tests

--- lib/leap_cli/config/object.rb
+++ lib/leap_cli/config/object.rb
@@ -1,5 +1,6 @@
 require 'erb'
 require 'json/pure'  # pure ruby implementation is required for our sorted trick to work.
+require 'ipaddr'
 
 $KCODE = 'UTF8' unless RUBY_VERSION > "1.9.0"
 require 'ya2yaml' # pure ruby yaml
