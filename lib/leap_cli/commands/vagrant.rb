aa2482f9a82af624a9af544b198c9adf654f0f6f
Micah Anderson <micah@riseup.net>
2012-12-11 11:32:42 -0500

    change name of vagrant basebox to 'leap-wheezy'

--- lib/leap_cli/commands/vagrant.rb
+++ lib/leap_cli/commands/vagrant.rb
@@ -109,8 +109,8 @@ module LeapCli; module Commands
     manager.each_node do |node|
       if node.vagrant?
         lines << %[  config.vm.define :#{node.name} do |config|]
-        lines << %[    config.vm.box = "minimal-wheezy"]
-        lines << %[    config.vm.box_url = "http://cloud.github.com/downloads/leapcode/minimal-debian-vagrant/minimal-wheezy.box"]
+        lines << %[    config.vm.box = "leap-wheezy"]
+        lines << %[    config.vm.box_url = "http://cloud.github.com/downloads/leapcode/minimal-debian-vagrant/leap-wheezy.box"]
         lines << %[    config.vm.network :hostonly, "#{node.ip_address}", :netmask => "#{netmask}"]
         lines << %[    config.vm.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]]
         lines << %[  end]
