a46321a43318a9cd3e2dd645b64fe81b71e7f8ea
elijah <elijah@riseup.net>
2013-06-02 23:41:39 -0700

    bump version to 1.0.0

--- lib/leap_cli/version.rb
+++ lib/leap_cli/version.rb
@@ -1,6 +1,6 @@
 module LeapCli
   unless defined?(LeapCli::VERSION)
-    VERSION = '0.3.0'
+    VERSION = '1.0.0'
     SUMMARY = 'Command line interface to the LEAP platform'
     DESCRIPTION = 'The command "leap" can be used to manage a bevy of servers running the LEAP platform from the comfort of your own home.'
     LOAD_PATHS = ['lib', 'vendor/certificate_authority/lib', 'vendor/rsync_command/lib']
