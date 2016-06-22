52dd4eb58bda84cb084cc119447cb83073b47510
elijah <elijah@riseup.net>
2016-06-21 17:48:07 -0700

    vendor base32 gem

--- lib/leap_cli/version.rb
+++ lib/leap_cli/version.rb
@@ -4,6 +4,10 @@ module LeapCli
     COMPATIBLE_PLATFORM_VERSION = '0.8'..'0.99'
     SUMMARY = 'Command line interface to the LEAP platform'
     DESCRIPTION = 'The command "leap" can be used to manage a bevy of servers running the LEAP platform from the comfort of your own home.'
-    LOAD_PATHS = ['lib', 'vendor/certificate_authority/lib', 'vendor/rsync_command/lib']
+    LOAD_PATHS = ['lib',
+      'vendor/certificate_authority/lib',
+      'vendor/rsync_command/lib',
+      'vendor/base32/lib'
+    ]
   end
 end
