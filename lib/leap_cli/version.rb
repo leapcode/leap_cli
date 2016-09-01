cc57bc6c0ff99d88f3bfeff1b04297e9b91e6988
elijah <elijah@riseup.net>
2016-09-01 13:24:45 -0700

    update version pinning for new platform release

--- lib/leap_cli/version.rb
+++ lib/leap_cli/version.rb
@@ -1,7 +1,7 @@
 module LeapCli
   unless defined?(LeapCli::VERSION)
-    VERSION = '1.8'
-    COMPATIBLE_PLATFORM_VERSION = '0.8'..'0.99'
+    VERSION = '1.8.1'
+    COMPATIBLE_PLATFORM_VERSION = '0.8'..'0.8.99'
     SUMMARY = 'Command line interface to the LEAP platform'
     DESCRIPTION = 'The command "leap" can be used to manage a bevy of servers running the LEAP platform from the comfort of your own home.'
     LOAD_PATHS = ['lib', 'vendor/certificate_authority/lib', 'vendor/rsync_command/lib']
